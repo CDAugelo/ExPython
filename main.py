b26e7b36792b35d78824475c99f43a8405fe645c
Bohdan <korogodskijbogdan@gmail.com>
2023-08-23 12:06:00 +0300

    +

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-name = input("Введи своё имя")
-print("Привет, ", name)
+name = "Augelo"
+print(name)
+
+isMarried = False
+print(isMarried)
+isAlive = True
+print(isAlive)
+
+age = 27
+print("Возраст ", age)
+count = 15
+print(count)
+
+user = f"name: {name}, age: {age}"
+print(user)
+
+userId = "abc"
+print(type(userId))
+
+userId = 234
+print(type(userId))
+
+print(type(str))
+print(type(int))
